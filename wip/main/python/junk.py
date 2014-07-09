083c1d75a5462da66d3bb33868c8dd723fbb75f4
Fred Eisele <feisele@isis.vanderbilt.edu>
2014-07-09 18:06:44 -0500

    Needed to add in the 'no' for making points
    not 0.0*n0

--- wip/main/python/junk.py
+++ wip/main/python/junk.py
@@ -16,11 +16,11 @@ print "quat: ", functions.exp(a)
 
 def make_conformal_point(euclid, bt):
   (no,e1,e2,e3,ni) = bt
-  return 0.0*no + euclid[0]*e1 + euclid[1]*e2 + euclid[2]*e3 +  0.5 *math.sqrt(euclid[0]**2 + euclid[1]**2 + euclid[2]**2)*ni
+  return no + euclid[0]*e1 + euclid[1]*e2 + euclid[2]*e3 +  0.5 *math.sqrt(euclid[0]**2 + euclid[1]**2 + euclid[2]**2)*ni
 
 def make_conformal_versor(euclid, axis, angle, bt):
   (no,e1,e2,e3,ni) = bt
-  return 0.0*no + euclid[0]*e1 + euclid[1]*e2 + euclid[2]*e3 +  0.5 *math.sqrt(euclid[0]**2 + euclid[1]**2 + euclid[2]**2)*ni
+  return no + euclid[0]*e1 + euclid[1]*e2 + euclid[2]*e3 +  0.5 *math.sqrt(euclid[0]**2 + euclid[1]**2 + euclid[2]**2)*ni
 
 b = make([1,0,0], bt)
 
