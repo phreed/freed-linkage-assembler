6c87c50a2f0024462eb0fd1ca59b68cee2a84a51
Fred Eisele <feisele@isis.vanderbilt.edu>
2014-07-15 11:37:21 -0500

    Added files for excavator tests

--- project.clj
+++ project.clj
@@ -4,19 +4,26 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clj"]
-  :test-paths ["src/test/clj"]
   ;; :java-source-paths ["src/main/java"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.2.1"]
                  [org.clojure/tools.logging "0.3.0"]
                  [expectations "2.0.7"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/data.zip "0.1.1"]]
   :plugins [[lein-expectations "0.0.7"]
             [lein-autoexpect "1.2.2"]
             [lein-ancient "0.5.5"]]
   :main isis.geom.linkage-assembler
   :target-path "target/%s"
-  :profiles {
-             :uberjar {:aot :all}
-             :dev {:aot :all} }
+  :dev-resources ["src/test/resources"]
+  :profiles
+  { :dev
+    {:aot :all
+     :test-paths ["src/test/clj"]
+     :resource-paths ["src/test/resources"]}
+
+    :uberjar
+    {:aot :all} }
+
   :aliases {"slamhound" ["run" "-m" "slam.hound"]} )
